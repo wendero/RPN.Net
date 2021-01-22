529c3557448c9a71b9503ff91ca0ec73dd942049
Wender <wendero@gmail.com>
2021-01-22 16:59:30 +0100

    Added RPN.Tool

---
+++ RPN.Tool/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.IO;
+
+namespace RPN.Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rootCommand = new RootCommand
+            {
+                new Argument<string>("expression", "RPN expression to be evaluated"),
+                new Option<List<string>>(new string[] {"p", "parameters"}, description: "List of parameters (primitive or json)"),
+                new Option<List<FileInfo>>(new string[] {"f", "files"}, description: "Files to be appended to parameters") { AllowMultipleArgumentsPerToken = false }
+            };
+
+            rootCommand.Description = "RPN Evaluator based on RPN.Net";
+
+            rootCommand.Handler = CommandHandler.Create<string, List<string>, List<FileInfo>>((expression, parameters, files) =>
+            {
+                var parsedParameters = new List<object>();
+                parsedParameters.AddRange(parameters);
+
+                files.ForEach(file =>
+                {
+                    parsedParameters.Add(file.OpenText().ReadToEnd());
+                });
+
+                var val = (RPN.Evaluate(new RPNExpression(expression, parsedParameters.ToArray())).ToString());
+                Console.WriteLine(val);
+            });
+
+            // Parse the incoming args and invoke the handler
+            rootCommand.InvokeAsync(args);
+        }
+    }
+}
